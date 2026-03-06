e8fda2b0c3c1d40c1398ea040cfee75e97d5f509
Elliot Fackler <grant.fackler48@gmail.com>
2026-03-05 22:06:07 -0600

    Improve evaulate function with config data

--- evaluator.py
+++ evaluator.py
@@ -1,26 +1,16 @@
 from config_loader import APP_CONFIG
-# Global variables
-CEILING_DICT = {'BTC-USD': 60000, 'ETH-USD': 2000}
-FLOOR_DICT = {'BTC-USD': 55000, 'ETH-USD': 1500}
-CEILING_GOAL_DICT = {'BTC-USD': False, 'ETH-USD': False}
-FLOOR_GOAL_DICT = {'BTC-USD': False, 'ETH-USD': False}
+
+#TODO: Convert variables in evaluator over to appconfig
 
 def evaluate_currency_prices(symbol, price):
     """Check whether cryptocurrency price has fallen below set floor price or risen above set ceiling price"""
-    global CEILING_DICT
-    global CEILING_GOAL_DICT
-    global FLOOR_DICT
-    global FLOOR_GOAL_DICT
 
-    #print("HERE")
-    #print(APP_CONFIG[symbol])
-
-    if (float(price) > CEILING_DICT.get(symbol) and CEILING_GOAL_DICT[symbol] == False):
-        CEILING_GOAL_DICT[symbol] = True
-        return "Ceiling", CEILING_GOAL_DICT.get(symbol)
-    elif (float(price) < FLOOR_DICT.get(symbol) and FLOOR_GOAL_DICT[symbol] == False):
-        FLOOR_GOAL_DICT[symbol] = True
-        return "Floor", FLOOR_GOAL_DICT.get(symbol)
+    if (float(price) > APP_CONFIG['watchlist'].get(symbol)['ceiling_target_price'] and APP_CONFIG['watchlist'].get(symbol)['alert_sent'] == 0):
+        APP_CONFIG['watchlist'].get(symbol)['alert_sent'] = 1
+        return "Ceiling", APP_CONFIG['watchlist'].get(symbol)['alert_sent']
+    elif (float(price) < APP_CONFIG['watchlist'].get(symbol)['floor_target_price'] and APP_CONFIG['watchlist'].get(symbol)['alert_sent'] == 0):
+        APP_CONFIG['watchlist'].get(symbol)['alert_sent'] = 1
+        return "Floor", APP_CONFIG['watchlist'].get(symbol)['alert_sent']
     else:
-        return "None", False
+        return "None", 0
         
--- ingestors/coinbase_ingestor.py
+++ ingestors/coinbase_ingestor.py
@@ -1,6 +1,7 @@
 import websockets
 import asyncio
 import json
+from collections import deque
 from config_loader import APP_CONFIG
 from messenger import build_email
 from evaluator import evaluate_currency_prices
@@ -9,38 +10,50 @@ async def stream_prices():
     """ Access Coinbase websocket and pull the currency data via a JSON block"""
     url = "wss://ws-feed.exchange.coinbase.com"
 
-    try:
-        async with websockets.connect(url) as websocket:      
-            print(f"Connected to live feed")
+    #price_buffers = { 
+    #    item['symbol']: deque(maxlen=300)
+    #    for item in APP_CONFIG['watchlist'] 
+    #} # Error: There is no watchlist currently in app_config
 
-            await websocket.send(json.dumps(APP_CONFIG['subscribe_message']))
-            print("Subscription sent successfully")
+    while True:
+        try:
+            async with websockets.connect(url) as websocket:      
+                print(f"Connected to live feed")
 
-            # Start stream and keep it going
-            while True:
-                response = await websocket.recv()
-                data = json.loads(response)
+                await websocket.send(json.dumps(APP_CONFIG['subscribe_message']))
+                print("Subscription sent successfully")
 
+                # Start stream and keep it going
+                while True:
+                    response = await websocket.recv()
+                    data = json.loads(response)
 
-                if(data.get('type') == 'ticker'):
-                    price = data.get('price')
-                    symbol = data.get('product_id')
+                    #print(data)
 
-                    # If the JSON has both the currency symbol and a price, print the data and evaluate buy option
-                    if symbol and price:
-                        level, value = evaluate_currency_prices(symbol, price)
+                    if(data.get('type') == 'ticker'):
+                        price = data.get('price')
+                        symbol = data.get('product_id')
 
-                        if (value == True):
-                            build_email(symbol, price, level)
+                        # If the JSON has both the currency symbol and a price, print the data and evaluate buy option
+                        if symbol and price:
+                            level, value = evaluate_currency_prices(symbol, price)
 
-                elif data.get('type') == 'heartbeat':
-                    # We don't need to print this, but it proves we're alive
-                    pass
-                else:
-                    print("Other types: ", data.get('type'))
+                            #print(APP_CONFIG['watchlist'].get(symbol)['ceiling_target_price'])
 
-    except websockets.exceptions.ConnectionClosed:
-        print("Connection failed") # Place to attempt reconnect
-        await asyncio.sleep(5)
-    except Exception as e:
-        print(f"Connection Ended: ${e}") # Error with reason
+                            #price_buffers[symbol].append(price)
+                            #print(price_buffers)
+
+                            if (value == 1):
+                                build_email(symbol, price, level)
+
+                    elif data.get('type') == 'heartbeat':
+                        # We don't need to print this, but it proves we're alive
+                        pass
+                    else:
+                        print("Other types: ", data.get('type'))
+
+        except websockets.exceptions.ConnectionClosed:
+            print("Connection failed") # Place to attempt reconnect
+            await asyncio.sleep(5)
+        except Exception as e:
+            print(f"Connection Ended: {e}") # Error with reason
